862579d661a19fbd344cc50b5ed0a3581a7b7a8d
rhernandez <rhernandez@bellbank.com>
2018-07-19 11:29:02 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Servidor de Bellbanking guarda las imagenes para que se puedan ver desde la calle.
+
+Ruta:
+https://bellbanking.bellbank.com/static/images/
+
+Tamaño de los iconos:
+        30 * 30
+
+Tamaño de la Imagen:
+        280 * 142
---
+++ static/style.js
@@ -0,0 +1,152 @@
+// Confirmar si el navegar es Mozilla
+$('#bFirma').ready(function() {
+	var url = "mensaje.html";
+	
+	if ($.browser.mozilla) {
+		$(location).attr('href',url);
+	//alert( 'Mozilla') ;
+	}
+});
+
+$('#bDescarga').click(function(){
+	var ruta = "./static/manual.pdf";
+	window.open(ruta, 'Manual');
+});
+
+
+//  Romulo
+//  Av. Rómulo Betancourt No. 2154, Renacimiento, Sto. Dgo. Rep. Dom.
+
+// Megacentro
+// 1er Nivel, Paseo de la Fauna, 11519, Sto. Dgo., Rep. Dom.
+
+// 27 de febrero
+// Av.27 de Febrero #452, Mirador Norte, Sto. Dgo., Rep. Dom.
+
+$(function() {
+
+	/* Nombre*/
+	$("#nombre").on('keyup', function(){
+		var value = $(this).val();
+
+		if($(this).val() !="")
+		{ 
+			$('#dnombre').removeClass('hide');
+			$("#snombre").html(value);
+		}
+		else
+		{
+			$('#dnombre').addClass('hide');
+		}
+	}).keyup();
+
+
+	/* Posicion */
+	$("#posicion").on('keyup', function(){
+		var value = $(this).val();
+		
+		if($(this).val() !="")
+		{ 
+			$('#dposicion').removeClass('hide');
+			$("#sposicion").html(value);
+		}
+		else{
+			$('#dposicion').addClass('hide');
+		}
+	}).keyup();
+
+
+	/* Departamento */
+	$("#departamento").on('keyup', function(){
+		var value = $(this).val();
+		if($(this).val() !="")
+		{ 
+			console.log($(this))           
+			$('#fdepartamento').removeClass('hide');
+			$("#xdepartamento").html(value);
+		}
+		else
+		{
+			$('#fdepartamento').addClass('hide');
+		}
+	}).keyup();
+
+
+	$('select#select').on('change',function(){
+	    var valor = $(this).val();
+	    var v_27f = "Av. 27 de Febrero #452, Mirador Norte, Sto. Dgo. Rep. Dom."
+		var v_meg = "Megacentro, Pasillo La Fauna, 1er nivel, local 46A, Sto. Dgo. Rep. Dom."
+		var v_rom = "Av. Rómulo Betancourt No. 2154, Renaciminto, Sto. Dgo. Rep. Dom."
+
+	    if(valor =="27 de Febrero")
+	    { 
+			$('#fdireccion').removeClass('hide');
+			$("#sdireccion").html(v_27f);
+		}
+		else if(valor =="Rómulo Betancourt")
+		{ 
+			$('#fdireccion').removeClass('hide');
+			$("#sdireccion").html(v_rom);
+		}
+		else if(valor =="Megacentro")
+		{ 
+			$('#fdireccion').removeClass('hide');
+			$("#sdireccion").html(v_meg);
+		}
+		else
+		{
+			$('#fdireccion').addClass('hide');
+		}
+	});
+
+
+	/* Extension */
+	$("#extension").on('keyup', function(){
+		var value = $(this).val();
+		if($(this).val() !="")
+		{ 
+			$('#fextension').removeClass('hide');
+			$("#sextension").html(value);
+		}
+		else
+		{
+			$('#fextension').addClass('hide');
+		}
+	}).keyup();
+
+
+	/* Flota */
+	$("#flota").on('keyup', function(){
+		var value = $(this).val();
+		
+		if($(this).val() !="")
+		{ 
+			$('#fflota').removeClass('hide');
+			$("#sflota").html(value);
+		}
+		else
+		{
+			$('#fflota').addClass('hide');
+		}
+	}).keyup();
+
+
+	/* Correo */
+	$("#correo").on('keyup', function(){
+		var value = $(this).val();
+		
+		if($(this).val() !="")
+		{ 
+			$('#fcorreo').removeClass('hide');
+			$("#scorreo").html(value);
+		}
+		else
+		{
+			$('#fcorreo').addClass('hide');        
+		}
+	}).keyup();
+
+
+
+});
+
